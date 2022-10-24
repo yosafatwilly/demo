cdbb42cf54345caf206f4b64490e4deec3ec905a
Yosafat Willy Christian <willychristt@gmail.com>
2022-10-24 23:10:35 +0700

    add thymeleaf

--- src/main/java/id/ac/usd/demo/DemoApplication.java
+++ src/main/java/id/ac/usd/demo/DemoApplication.java
@@ -2,16 +2,20 @@ package id.ac.usd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+	@GetMapping("/")
+	public String showHomePage(Model model) {
+		model.addAttribute("pageName", "HomePage");
+		return "homepage";
 	}
 
 	public static void main(String[] args) {
